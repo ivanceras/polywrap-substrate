3eae325ac61cce63827cc390112894be4f208a23
Jovansonlee Cesar <ivanceras@gmail.com>
2022-06-03 00:36:19 +0800

    refactor: improve the graphql api returning a result, so the client can see the error when something is wrong along the api call stack

--- graphql-substrate/src/lib.rs
+++ graphql-substrate/src/lib.rs
@@ -28,36 +28,29 @@ impl ChainApi {
         ChainApi
     }
 
-    pub async fn block(&self, number: u32) -> Option<BlockDetail> {
+    pub async fn block(&self, number: u32) -> Result<Option<BlockDetail>, mycelium::Error> {
         let block = Api::new("http://localhost:9933")
             .fetch_block::<Block>(number)
-            .await
-            .ok()
-            .flatten();
-        block.map(|block| BlockDetail {
-            number: block.header.number.to_string(),
-            header: Header {
-                parent_hash: block.header.parent_hash.to_string(),
-                state_root: block.header.state_root.to_string(),
-                extrinsics_root: block.header.extrinsics_root.to_string(),
-            },
-        })
+            .await?;
+        match block {
+            Some(block) => Ok(Some(BlockDetail {
+                number: block.header.number.to_string(),
+                header: Header {
+                    parent_hash: block.header.parent_hash.to_string(),
+                    state_root: block.header.state_root.to_string(),
+                    extrinsics_root: block.header.extrinsics_root.to_string(),
+                },
+            })),
+            None => Ok(None),
+        }
     }
 
-    pub async fn metadata(&self) -> Option<mycelium::Metadata> {
-        let metadata = Api::new("http://localhost:9933")
-            .fetch_metadata()
-            .await
-            .expect("must not error");
-        dbg!(&metadata);
-        Some(metadata)
+    pub async fn metadata(&self) -> Result<Option<mycelium::Metadata>, mycelium::Error> {
+        Api::new("http://localhost:9933").fetch_metadata().await
     }
 
-    pub async fn rpc_methods(&self) -> Option<Vec<String>> {
-        Api::new("http://localhost:9933")
-            .fetch_rpc_methods()
-            .await
-            .ok()
+    pub async fn rpc_methods(&self) -> Result<Vec<String>, mycelium::Error> {
+        Api::new("http://localhost:9933").fetch_rpc_methods().await
     }
 }
 
@@ -69,15 +62,18 @@ impl QueryRoot {
         &self,
         ctx: &Context<'a>,
         #[graphql(desc = "the block number")] number: u32,
-    ) -> Option<BlockDetail> {
+    ) -> Result<Option<BlockDetail>, mycelium::Error> {
         ctx.data_unchecked::<ChainApi>().block(number).await
     }
 
-    async fn metadata<'a>(&self, ctx: &Context<'a>) -> Option<mycelium::Metadata> {
+    async fn metadata<'a>(
+        &self,
+        ctx: &Context<'a>,
+    ) -> Result<Option<mycelium::Metadata>, mycelium::Error> {
         ctx.data_unchecked::<ChainApi>().metadata().await
     }
 
-    async fn rpc_methods<'a>(&self, ctx: &Context<'a>) -> Option<Vec<String>> {
+    async fn rpc_methods<'a>(&self, ctx: &Context<'a>) -> Result<Vec<String>, mycelium::Error> {
         ctx.data_unchecked::<ChainApi>().rpc_methods().await
     }
 }
